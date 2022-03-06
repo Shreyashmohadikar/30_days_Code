ee16b2e67444f6fb800e1ae8c684ba1543d5580a
Shreyashmohadikar <shreyashmohadikar@gmail.com>
2022-03-06 20:58:16 +0530

    D11

---
+++ D11.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[7][7];
+int sum(int x, int y){
+    return arr[x][y] + arr[x][y+1] + arr[x][y+2] 
+                 + arr[x+1][y+1] 
+        + arr[x+2][y] + arr[x+2][y+1] + arr[x+2][y+2];
+}
+
+int main(){
+    for(int i=0; i<6; i++){
+        for(int j=0; j<6; j++){
+            cin>>arr[i][j];
+        }
+    }
+    int ans=0;
+    for(int i=0; i<4; i++){
+        for(int j=0; j<4; j++){
+            ans = max(ans, sum(i,j));
+        }
+    }
+    cout<<ans;
+    return 0;
+}
