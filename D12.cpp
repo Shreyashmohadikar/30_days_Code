5c0629c5e30e980f2a7f8e4a523b8b267d2b217f
Shreyashmohadikar <shreyashmohadikar@gmail.com>
2022-03-09 21:40:43 +0530

    D12

---
+++ D12.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Person{
+
+};
+
+class Student : public Person{
+    private:
+        vector<int> testscores;
+    public:
+    //constructor
+    Student(string firstname, string lastname
+    int idNumber, vector<int> scores){
+        
+    }
+
+    char calculate(){
+        int total = 0;
+        for (int i=0; i<this->testscores.size; i++){
+            total += this->testscores[i];
+        }
+        int average_marks = (total/this->testscores.size());
+        
+        // cout<<"Grade:"<<average_marks;
+    }
+};
+
+int main(){
+    int num;
+    cin>>num;
+    int arr[num];
+    int sum=0;
+    for (int i=0; i<num; i++){
+        cin>>arr[i];
+    }
+    calculate(arr[num]);
+
+
+    return 0;
+}
