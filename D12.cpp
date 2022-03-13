07145a20624c21c5ceffeb7172e0969ecb0a08d1
Shreyashmohadikar <shreyashmohadikar@gmail.com>
2022-03-13 16:17:19 +0530

    D132

--- D12.cpp
+++ D12.cpp
@@ -1,41 +1,81 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
+
 class Person{
-
+	protected:
+		string firstName;
+		string lastName;
+		int id;
+	public:
+		Person(string firstName, string lastName, int identification){
+			this->firstName = firstName;
+			this->lastName = lastName;
+			this->id = identification;
+		}
+		void printPerson(){
+			cout<< "Name: "<< lastName << ", "<< firstName <<"\nID: "<< id << "\n"; 
+		}
+	
 };
 
-class Student : public Person{
-    private:
-        vector<int> testscores;
-    public:
-    //constructor
-    Student(string firstname, string lastname
-    int idNumber, vector<int> scores){
+class Student :  public Person{
+	private:
+		vector<int> testScores;  
+	public:    
+        /*	
+        *   Class Constructor
         
-    }
-
-    char calculate(){
-        int total = 0;
-        for (int i=0; i<this->testscores.size; i++){
-            total += this->testscores[i];
+        *   
+        *   Parameters:
+        *   firstName - A string denoting the Person's first name.
+        *   lastName - A string denoting the Person's last name.
+        *   id - An integer denoting the Person's ID number.
+        *   scores - An array of integers denoting the Person's test scores.
+        */
+        // Write your constructor here
+        Student(string firstName, string lastName, int id, vector<int> scores) : Person(firstName, lastName, id){
+            for(int i=0; i<scores.size(); i++){
+                testScores.push_back(scores[i]);
+            }
         }
-        int average_marks = (total/this->testscores.size());
+        /*	
+        *   Function Name: calculate
+        *   Return: A character denoting the grade.
+        */
+        // Write your function here
+        char calculate() const{
+        int grade = 0;
+        for (int i=0; i<testScores.size(); i++){
+            grade += testScores[i];
+        }
+        grade /= testScores.size();
+        if (grade >= 90 && grade <= 100) return 'O';
+        else if (grade >= 80 && grade < 90) return 'E';
+        else if (grade >= 70 && grade < 80) return 'A';
+        else if (grade >= 55 && grade < 70) return 'P';
+        else if (grade >= 40 && grade < 55) return 'D';
+        else return 'T';        
 
-        // cout<<"Grade:"<<average_marks;
     }
 };
 
-int main(){
-    int num;
-    cin>>num;
-    int arr[num];
-    int sum=0;
-    for (int i=0; i<num; i++){
-        cin>>arr[i];
-    }
-    calculate(arr[num]);
-
-
-    return 0;
+int main() {
+	string firstName;
+  	string lastName;
+	int id;
+  	int numScores;
+	cin >> firstName >> lastName >> id >> numScores;
+  	vector<int> scores;
+  	for(int i = 0; i < numScores; i++){
+	  	int tmpScore;
+	  	cin >> tmpScore;
+		scores.push_back(tmpScore);
+	}
+	Student* s = new Student(firstName, lastName, id, scores);
+	s->printPerson();
+	cout << "Grade: " << s->calculate() << "\n";
+	return 0;
 }
