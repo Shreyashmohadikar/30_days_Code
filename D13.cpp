4927e6b04d65d373c51c160c1e4c84e43aba42d3
Shreyashmohadikar <shreyashmohadikar@gmail.com>
2022-03-14 19:36:29 +0530

    D13

--- D13.cpp
+++ D13.cpp
@@ -14,27 +14,18 @@ class Difference {
   	int maximumDifference;
 
 	// Add your code here
-    
-}; // End of Difference class
-
-int main() {
-    int N;
-    cin >> N;
-    
-    vector<int> a;
-    
-    for (int i = 0; i < N; i++) {
-        int e;
-        cin >> e;
-        
-        a.push_back(e);
+    Difference(vector<int> a){
+        elements = a;
+        maximumDifference = 0;
     }
-    
-    Difference d(a);
-    
-    d.computeDifference();
-    
-    cout << d.maximumDifference;
-    
-    return 0;
-}
+    void computeDifference(){
+        int n = elements.size() - 1;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(maximumDifference < abs(elements[i] - elements[j+1])){
+                    maximumDifference = abs(elements[i] - elements[j+1]);
+                }
+            }
+        }
+    }
+}; // End of Difference class
