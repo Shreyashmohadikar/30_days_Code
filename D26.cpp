7c74c15ff4a9c5922c65faf7e3803015a4df078f
Shreyashmohadikar <shreyashmohadikar@gmail.com>
2022-03-22 16:32:23 +0530

    D26

--- D26.cpp
+++ D26.cpp
@@ -1,3 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int RD, DD;
+    int RM, DM;
+    int RY, DY;
+    int fine = 0;
+
+    cin>>RD>>RM>>RY;
+    cin>>DD>>DM>>DY;
+
+    if(RY < DY && DY <= RY){
+        fine = 0;
+        cout<<fine;
+    }
+    else if(RD > DD && DM == RM && DY == RY ){
+        fine = 15 * abs(RD - DD);
+    }
+    else if(RD > DD && DY == RY && DM != RM){
+        fine = 500 * abs(RM - DM);
+    }
+    else if((RY - DY) > 0){
+        fine = 10000;
+    }
+    cout<<fine;
+
+    return 0;
+}
+
+
 // #include <bits/stdc++.h>
 // using namespace std;
 
@@ -31,32 +62,3 @@
 //     return 0;
 // }
 
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    int RD, DD;
-    int RM, DM;
-    int RY, DY;
-    int fine = 0;
-
-    cin>>RD>>RM>>RY;
-    cin>>DD>>DM>>DY;
-
-    if(RY < DY && DY <= RY){
-        fine = 0;
-        cout<<fine;
-    }
-    else if(RD > DD && DM == RM && DY == RY ){
-        fine = 15 * abs(RD - DD);
-    }
-    else if(RD > DD && DY == RY && DM != RM){
-        fine = 500 * abs(RM - DM);
-    }
-    else if((RY - DY) > 0){
-        fine = 10000;
-    }
-    cout<<fine;
-
-    return 0;
-}
