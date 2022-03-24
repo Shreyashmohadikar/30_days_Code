5c112dd6630f0f1370b229705d2f6fadc575cd65
Shreyashmohadikar <shreyashmohadikar@gmail.com>
2022-03-24 15:55:22 +0530

    D28

--- D28.cpp
+++ D28.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main(){
+    int t;
+    cin>>t;
+    string FN, emailId; 
+    vector<string> v;
+    string str ("@gmail.com");
+    while(t--){
+        cin>>FN>>emailId;
+        if (emailId.find(str) != string::npos) 
+        {
+            v.push_back(FN);
+            // cout<<FN<<endl;
+        }
+    }
+    sort(v.begin(), v.end());
+ 
+    for (auto x : v)
+        cout << x << endl;
     
 
     return 0;
